e857ac2f4b487e3b4097fc074831514a23e6ac9c
Krzysztof Wilk <krzysztof.wilk@solidbrain.com>
2016-09-05 11:46:41 +0200

    Tests that verify correctness of users pass

--- src/test/groovy/com/solidbrain/MilestoneOneTest.groovy
+++ src/test/groovy/com/solidbrain/MilestoneOneTest.groovy
@@ -5,6 +5,8 @@ package com.solidbrain;
  */
 
 import com.getbase.Configuration
+import com.getbase.models.User
+import com.getbase.services.UsersService
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -40,7 +42,7 @@ public class MilestoneOneTest extends Specification {
         return System.getenv("ADMIN_EMAIL")
     }
 
-    def "admin user works"() {
+    def "admin user exists"() {
         given:
             context != null
             baseClient instanceof Client
@@ -48,10 +50,56 @@ public class MilestoneOneTest extends Specification {
         when: "Fetching list of users"
             def response = baseClient.users()
 
-        then: "Verifying if admin user is available"
+        then: "Verifying if admin user exists"
             def adminEmail = getAdminEmail()
             response.self().email == adminEmail
             response.self().confirmed
-            response.self().role == 'admin'
-        }
+            response.self().role == "admin"
+            response.self().status == "active"
+    }
+
+    def getAccountManagerEmail() {
+        return "chrismwilk+accountmanager+sara@gmail.com"
+    }
+
+    def "account manager exists"() {
+        given:
+        context != null
+        baseClient instanceof Client
+
+        when: "Fetching list of users"
+        def response = baseClient.users()
+        def amEmailsPattern = "\\+accountmanager\\+"
+        def amUsers = response.list(new UsersService.SearchCriteria().
+                                                        asMap()).
+                                findAll { it.role == "user" &&
+                                            it.status == "active" &&
+                                            it.email =~ amEmailsPattern}
+
+        then: "Verifying if account manager exists"
+        amUsers.size() == 1
+        amUsers.get(0).email == accountManagerEmail
+        amUsers.get(0).confirmed
+        amUsers.get(0).status == "active"
+    }
+
+    def "sales representatives exist"() {
+        given:
+        context != null
+        baseClient instanceof Client
+
+        when: "Fetching list of users"
+        def response = baseClient.users()
+        def salesRepEmailsPattern = "\\+salesrep\\+"
+        def salesRepUsers = response.list(new UsersService.SearchCriteria().
+                                                                asMap()).
+                                    findAll { it.role == "user" &&
+                                                it.status == "active" &&
+                                                it.email =~ salesRepEmailsPattern}
+
+        then: "Verifying if sales representatives exist"
+        salesRepUsers.size() == 3
+        salesRepUsers.forEach { it.confirmed == true}
+        salesRepUsers.forEach { it.status == "active"}
+    }
 }
