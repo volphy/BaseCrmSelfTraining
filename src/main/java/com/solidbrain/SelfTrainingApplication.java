411b036b71058cb4be705cbfde36e6dbf4bf52f7
Krzysztof Wilk <krzysztof.wilk@solidbrain.com>
2016-09-13 12:54:55 +0200

    Spring Boot main entry class needs to be public in order to make this application run properly from command line

--- src/main/java/com/solidbrain/SelfTrainingApplication.java
+++ src/main/java/com/solidbrain/SelfTrainingApplication.java
@@ -4,14 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * Spring Boot application entry class
+ */
 @SpringBootApplication
 @EnableScheduling
 @SuppressWarnings("squid:S1118")
-class SelfTrainingApplication {
+public class SelfTrainingApplication {
 
     /**
-     * Spring Boot application entry point
-     * @param args
+     * Spring Boot application entry point method
+     * @param args arguments (currently not used)
      */
     // https://jira.sonarsource.com/browse/SONARJAVA-1687
 	@SuppressWarnings("squid:S2095")
